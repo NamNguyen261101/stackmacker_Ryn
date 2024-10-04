4159c6a9c1d6c378616845c160e8701150a0f959
Rynn <nguyenkhanhnam2001.12@gmail.com>
2024-10-04 03:34:41 -0600

    Update Camera Holder

--- Stackmacker/Assets/Game/001 Scripts/Core/Camera/CameraHolder.cs
+++ Stackmacker/Assets/Game/001 Scripts/Core/Camera/CameraHolder.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class CameraHolder : MonoBehaviour
 {
-    #region Variables
+    public Transform TargetObject;
+    public float MoveSmoothTime = 0.3F;
+    public float RotationSpeed = 6f;
+    public Vector3 Offset = new Vector3(0f, 15f, 0f);
 
-    private Vector3 _offset;
-    [SerializeField] private Transform target;
-    [SerializeField] private float smoothTime;
-    private Vector3 _currentVelocity = Vector3.zero;
+    public bool Enabled = true;
 
-    #endregion
+    private Transform myTransform;
+    private Vector3 velocity = Vector3.zero;
 
-    #region Unity callbacks
-
-    private void Awake() => _offset = transform.position - target.position;
-
-    private void LateUpdate()
+    void Awake()
     {
-        Vector3 targetPosition = target.position + _offset;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, smoothTime);
+        myTransform = this.transform;
     }
 
-    #endregion
+    void Update()
+    {
+        if (TargetObject != null && Enabled)
+        {
+            Vector3 targetPosition = TargetObject.TransformPoint(Offset);
+            myTransform.position = Vector3.SmoothDamp(myTransform.position, targetPosition, ref velocity, MoveSmoothTime);
+            if (RotationSpeed > 0)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(TargetObject.position - myTransform.position);
+                this.transform.rotation = Quaternion.Slerp(myTransform.rotation, targetRotation, Time.deltaTime * RotationSpeed);
+            }
+        }
+    }
 }
